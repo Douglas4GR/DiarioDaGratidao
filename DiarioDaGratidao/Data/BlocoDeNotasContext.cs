c45fe60187e822695d9b378d2d16a041f653d531
Douglas Garcia Rangel <109640187+Douglas4GR@users.noreply.github.com>
2024-06-25 19:58:18 -0300

    Criando primeiras estruturas do banco de dados

---
+++ DiarioDaGratidao/Data/BlocoDeNotasContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DiarioDaGratidao.Models;
+
+namespace DiarioDaGratidao.Data
+{
+    public class BlocoDeNotasContext : DbContext
+    {
+        public BlocoDeNotasContext(DbContextOptions<BlocoDeNotasContext> options) : base(options) { }
+
+        public DbSet<Nota> Notas { get; set; }
+    }
+}
---
+++ DiarioDaGratidao/Models/Nota.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DiarioDaGratidao.Models
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
